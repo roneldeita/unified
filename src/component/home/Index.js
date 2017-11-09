2859373ac10207220f2daf0ac6d2a5c89495cb8f
Ronel Deita <ortigas.ronel@gmail.com>
2017-11-09 19:12:36 +0800

    Navigation inspired from paypal navigation

--- src/component/home/Index.js
+++ src/component/home/Index.js
@@ -1,17 +1,60 @@
 import React from 'react'
 import '../../assets/style/home.css'
+import Icon1 from '../../assets/images/icon1.png'
+import Icon2 from '../../assets/images/icon2.png'
+import Icon3 from '../../assets/images/icon3.png'
 
 export default class Index extends React.Component{
-
   render(){
     return(
-      <div className="container-fluid cover">
-        <div className="row">
-          <div className="col-sm-12 col-md-6" style={{height:300}}>
-
+      <div>
+        <div className="container-fluid cover-container">
+          <div className="container cover">
+            <div className="row">
+              <div className="col-md-offset-1 col-md-5">
+                <p className="slogan">We empower you to attain <br />
+                  financial freedom and <br />
+                  economic stability.</p>
+                  <br />
+                  <p className="slogan-sub">
+                  UPS offers services that you cannot avail from anyone else.<br />
+                  With Unified Producs and Services you can buy load,<br />
+                  pay your bills, book flights online, shopping all day<br />
+                  and send and receive money anywhere in the world.<br />
+                  All from your mobile phone!
+                  </p>
+                  <br />
+                  <a href="/" className="btn btn-learn-more">Learn More</a>
+              </div>
+              <div className="col-sm-12 col-md-5 cover-vid">
+                <iframe width="400" height="227" title="Unified Video" className="pull-right" src="https://www.youtube.com/embed/4Zqjtqntx8Q?controls=0" frameBorder="0" gesture="media" allowFullScreen></iframe>
+              </div>
+            </div>
           </div>
-          <div className="col-sm-12 col-md-6">
-            
+        </div>
+        <div className="container-fluid" style={{backgroundColor:"#ebf2fa"}}>
+          <div className="container" style={{marginTop:60, marginBottom:60}}>
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 style={{marginBottom:70}}><b>Why use Unified?</b></h1>
+              </div>
+              <div className="col-md-offset-1 col-md-10 text-center">
+                <div className="row">
+                  <div className=" col-md-4">
+                    <img alt="icon 1" src={Icon1} className="icon" />
+                    <h4>You can pay derictly anywhere</h4>
+                  </div>
+                  <div className="col-md-4">
+                    <img alt="icon 2" src={Icon2} className="icon" />
+                    <h4>Trusted business nationwide</h4>
+                  </div>
+                  <div className="col-md-4">
+                    <img alt="icon 3" src={Icon3} className="icon" />
+                    <h4>Cashless payments</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/component/template/Navigation.js
+++ src/component/template/Navigation.js
@@ -6,33 +6,54 @@ import '../../assets/style/navigation.css'
 export default class Navigation extends React.Component{
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {
+      navClass:"navbar-default"
+    };
   };
+
+  componentDidMount(){
+    window.addEventListener('scroll', this.handleScroll.bind(this))
+  }
+
+  handleScroll(){
+    var scrollX = window.pageYOffset;
+    const Navclass = scrollX <= 50 ? 'navbar-default' : 'navbar-default-active';
+    console.log(scrollX);
+    console.log(Navclass);
+    this.setState({navClass:Navclass})
+  }
 
   render() {
     return (
-        <nav className="navbar navbar-default navbar-fixed-top">
+        <nav className={`navbar ${this.state.navClass} navbar-fixed-top`}>
           <div className="container">
-            <div className="navbar-header">
-              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span className="sr-only">Toggle navigation</span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-              </button>
-              <NavLink to="/" className="navbar-brand"><img src={Logo} className="logo" alt="logo"/></NavLink>
-            </div>
-            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-              <ul className="nav navbar-nav navbar-right">
-                <li><Link className="btn btn-success" to="/">Login</Link></li>
-                <li><Link to="/">Register</Link></li>
-              </ul>
-              <ul className="nav navbar-nav navbar-right">
-                <li><NavLink exact activeClassName="active" to="/">Home</NavLink></li>
-                <li><NavLink activeClassName="active" to="/about">About</NavLink></li>
-                <li><NavLink activeClassName="active" to="/todos">Todos</NavLink></li>
-                <li><NavLink activeClassName="active" to="/tictactoe">Tic-Tac-Toe</NavLink></li>
-              </ul>
+            <div className="row">
+              <div className="col-md-offset-1 col-md-10">
+                <div className="navbar-header">
+                  <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span className="sr-only">Toggle navigation</span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                  </button>
+                  <NavLink to="/" className="navbar-brand"><img src={Logo} className="logo" alt="logo"/></NavLink>
+                </div>
+                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <ul className="nav navbar-nav navbar-right">
+                    <li><Link to="/" className="space"></Link></li>
+                    <li><Link className="btn btn-login" to="/">Login</Link></li>
+                  </ul>
+                  <ul className="nav navbar-nav navbar-right">
+                    <li><NavLink exact activeClassName="active" to="/">HOME</NavLink></li>
+                    <li><NavLink activeClassName="active" to="/a">PRODUCTS</NavLink></li>
+                    <li><NavLink activeClassName="active" to="/b">SERVICES</NavLink></li>
+                    <li><NavLink activeClassName="active" to="/about">ABOUT</NavLink></li>
+                    <li><NavLink activeClassName="active" to="/c">CONTACT</NavLink></li>
+                    {/*<li><NavLink activeClassName="active" to="/todos">Todos</NavLink></li>
+                    <li><NavLink activeClassName="active" to="/tictactoe">Tic-Tac-Toe</NavLink></li>*/}
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
         </nav>
